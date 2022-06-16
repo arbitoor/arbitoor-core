61b1eedef6bd317123b5059dcd17863a17a6fcaa
zkwarlock <shardul.aeer@somaiya.edu>
2022-06-16 17:37:58 +0530

    fix: Token registration amount

--- sdk/constants.ts
+++ sdk/constants.ts
@@ -1,3 +1,3 @@
-export const STORAGE_TO_REGISTER_WITH_MFT = '1'
+export const STORAGE_TO_REGISTER_WITH_MFT = '100000000000000000000000'
 export const REF = 'v2.ref-finance.near'
 export const JUMBO = 'v1.jumbo_exchange.near'
